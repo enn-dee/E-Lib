867394a14a2bc5341cc52140f6743db63ac0d5b5
enn-dee <nddar191131@gmail.com>
2025-04-11 11:23:49 +0530

    DB setup

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,7 @@ import { config as conf } from "dotenv";
 conf();
 const _config = {
    port: process.env.PORT,
+   database_url: process.env.DB_URL,
 };
 
 export const config = Object.freeze(_config);
---
+++ src/config/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+
+const connectDB = async () => {
+   try {
+      mongoose.connection.on("connected", () => {
+         console.log("======Connected to DB======");
+      });
+
+      mongoose.connection.on("error", (error) => {
+         console.log("Error in connecting to db: ", error);
+      });
+
+      await mongoose.connect(config.database_url as string);
+   } catch (error) {
+      console.log("Error connecting to db: ", error);
+      process.exit(1);
+   }
+};
+
+export default connectDB;
