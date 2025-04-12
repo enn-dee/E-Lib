00827b8f956fd2b2608b5e1de4e413284e5fecbd
enn-dee <nddar191131@gmail.com>
2025-04-12 21:00:35 +0530

    feat: user login functionality

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -44,10 +44,29 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
          algorithm: "HS256",
       });
 
-      res.status(200).json({ message: "User created", accessToken: token });
+      res.status(201).json({ message: "User created", accessToken: token });
    } catch (err) {
       return next(createHttpError(500, "Error while signing the jwt token"));
    }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+   const { email, password } = req.body;
+   if (!email || !password)
+      return next(createHttpError(400, "All fields are required"));
+   try {
+      const user = await userModel.findOne({ email });
+      if (!user) return next(createHttpError(404, "User not found"));
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return next(createHttpError(401, "Invalid Password"));
+
+      const token = sign({ sub: user._id }, config.jwtSecret as string, {
+         expiresIn: "7d",
+         algorithm: "HS256",
+      });
+      res.status(200).json({ message: "Logged In", accessToken: token });
+   } catch (err) {
+      return next(createHttpError(500, "Error while fetching user"));
+   }
+};
+export { createUser, loginUser };
