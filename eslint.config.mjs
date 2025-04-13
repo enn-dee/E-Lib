794da8aa5297cdf466a3fd80865f43cb8c9688d4
enn-dee <nddar191131@gmail.com>
2025-04-13 21:24:40 +0530

    eslint: Blank line b/w specific types of statements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,22 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+   {
+      files: ["**/*.{js,mjs,cjs,ts}"],
+      plugins: { js },
+      extends: ["js/recommended"],
+      rules: {
+         "padding-line-between-statements": [
+            "error",
+            { blankLine: "always", prev: "*", next: "*" },
+            { blankLine: "any", prev: "import", next: "import" },
+         ],
+      },
+   },
+   {
+      files: ["**/*.{js,mjs,cjs,ts}"],
+      languageOptions: { globals: globals.browser },
+   },
+   tseslint.configs.recommended,
 ]);
